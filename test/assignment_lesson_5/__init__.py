a3a4e838f242c6fa00df0dd476b2d0f187f39d7a
Arthur <shoshoavi2@gmail.com>
2024-01-05 17:19:34 +0200

    created Minesweeper

---
+++ test/assignment_lesson_5/__init__.py
@@ -0,0 +1,8 @@
+user_age = int(input('please insert your age: '))
+
+if user_age >= 30:
+    print('you_are_old...')
+elif 20< user_age <30:
+    print("it's the time of your life")
+else:
+    print('you are young')
