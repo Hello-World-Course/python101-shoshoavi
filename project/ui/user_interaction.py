1815ca92eff88ffb3aaa9f47829b2cce1a5a4b74
Arthur <shoshoavi2@gmail.com>
2024-01-05 16:03:34 +0200

    created Minesweeper

---
+++ project/ui/user_interaction.py
@@ -0,0 +1,4 @@
+name = input("Hello, what's your name? ")
+board_size = int(input(name + ", please choose board size: "))
+number_of_mines = int(input(name + ", for board size " + str(board_size) + ", choose number of mines to allocate: "))
+print(name + ", the board size is: " + str(board_size) + ", number of mines is: " + str(number_of_mines) + ". ENJOY!")
